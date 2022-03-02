5ca3b72e36623653567e1c26e0a99ccdd96ff5a0
EmdadHusain <100661175+EmdadHusain@users.noreply.github.com>
2022-03-03 05:23:36 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".allSliders").bxSlider();
+    $(".main-menu").meanmenu({
+        meanMenuContainer:'#mobile-menu',
+        meanScreenWidth:767,
+    })
+    $(".video-play-btn").magnificPopup({
+        type:'video',
+    })
+})
